9f5b4df37c64aae8d1e09ced73b569d11f3e4950
bryancatanigama <bryancatani@gmail.com>
2025-11-11 10:26:42 -0300

    feat: altera mensagem da home para testar deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, mundo! Projeto gustavo-teste rodando 🚀');
+  res.send('Hello, mundo! Deploy automático do deploy-pipeline-teste 🚀');
 });
 
 app.listen(PORT, () => {
